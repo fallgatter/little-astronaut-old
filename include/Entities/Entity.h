5481294117603076e40e71a31e4881e1318606e1
Fallgatter <fallgatter@alunos.utfpr.edu.br>
2023-05-09 19:01:31 -0300

    entity git fix

--- include/Entities/Entity.h
+++ include/Entities/Entity.h
@@ -16,13 +16,9 @@ namespace Entities{
             Entity(int X=0, int Y=0, Vector2f SIZE=(0.0,0.0), Vector2f POS=(0.0,0.0), int ID);
             virtual ~Entity();
             virtual void run();
-<<<<<<< HEAD
             void setSize(Vector2f SIZE=(0.0,0.0)) const;
             void setPosition(Vector2f POS=(0.0,0.0)) const;
             Vector2f getSize();
             Vector2f getPosition();
-=======
-    
->>>>>>> bdc5bae1b1a30d74ee64dd0bc935b29e544fe0c7
     };
 }using namespace Entities;
